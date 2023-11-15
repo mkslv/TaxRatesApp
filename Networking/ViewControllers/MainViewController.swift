43fcafdeef7e6e3c2244a896a4b714218c6574a0
Max Kiselyov <77746941+mkslv@users.noreply.github.com>
2023-11-15 16:09:55 +0000

    Main VC button and layout changed

--- Networking/ViewControllers/MainViewController.swift
+++ Networking/ViewControllers/MainViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class MainViewController: UIViewController {
-    
+    private let infoLabel = UILabel()
     private let countryPicker = UIPickerView()
     private let nextButton = UIButton()
     
@@ -56,6 +56,7 @@ private extension MainViewController {
         addSubviews()
         view.backgroundColor = .systemBackground
         setupButton()
+        setupLabel()
         
         setupLayout()
     }
@@ -63,7 +64,7 @@ private extension MainViewController {
 
 private extension MainViewController {
     func addSubviews() {
-        [countryPicker, nextButton].forEach { subView in
+        [infoLabel, countryPicker, nextButton].forEach { subView in
             view.addSubview(subView)
         }
     }
@@ -74,22 +75,34 @@ private extension MainViewController {
         nextButton.setTitleColor(.systemMint, for: .normal)
         nextButton.setTitleColor(.systemMint.withAlphaComponent(0.6), for: .focused)
         nextButton.backgroundColor = .black
+        nextButton.layer.cornerRadius = 30
     }
     
+    func setupLabel() {
+        infoLabel.text = "Choose the country and get info about VAT and Sales Taxes in the region"
+        infoLabel.numberOfLines = 0
+        
+    }
 }
 
 private extension MainViewController {
     func setupLayout() {
-        [countryPicker, nextButton].forEach { subView in
+        [infoLabel, countryPicker, nextButton].forEach { subView in
             subView.translatesAutoresizingMaskIntoConstraints = false
         }
         
         NSLayoutConstraint.activate([
+            infoLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            infoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            infoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            
             countryPicker.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            countryPicker.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            countryPicker.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 100),
             
             nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -200)
+            nextButton.topAnchor.constraint(equalTo: countryPicker.bottomAnchor, constant: 100),
+            nextButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.66),
+            nextButton.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
 }
